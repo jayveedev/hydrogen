176754d01fd0c0f70683456c306b8f788f9634c8
Jayvee <jayvee.acar.dev@gmail.com>
2023-07-15 22:39:28 +0800

    basic template

---
+++ app/routes/_index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export function meta() {
+  return [
+    {
+      title: 'Hydrogen Wuffes',
+    },
+    {
+      description: 'A custom storefront powered by Hydrogen',
+    },
+  ];
+}
+
+export default function Index() {
+  return (
+    <div>
+      <h3>Hello world!</h3>
+    </div>
+  );
+}
